0942e12727992fa46aee9a57b0773356b1cd2bf3
Garreth Botha <garrethowenbotha@gmail.com>
2020-09-30 11:34:41 +0200

    Added a form that displays the path of a selected folder and writes it to a text file

---
+++ Activity_4.1.1/FileViewerForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Activity_4._1._1
+{
+    partial class FileViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnOpenFolderBrowser = new System.Windows.Forms.Button();
+            this.rtbFolderViewer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolderBrowser
+            // 
+            this.btnOpenFolderBrowser.Location = new System.Drawing.Point(282, 57);
+            this.btnOpenFolderBrowser.Name = "btnOpenFolderBrowser";
+            this.btnOpenFolderBrowser.Size = new System.Drawing.Size(181, 32);
+            this.btnOpenFolderBrowser.TabIndex = 0;
+            this.btnOpenFolderBrowser.Text = "Open Folder Browser";
+            this.btnOpenFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnOpenFolderBrowser.Click += new System.EventHandler(this.btnOpenFolderBrowser_Click);
+            // 
+            // rtbFolderViewer
+            // 
+            this.rtbFolderViewer.Location = new System.Drawing.Point(118, 292);
+            this.rtbFolderViewer.Name = "rtbFolderViewer";
+            this.rtbFolderViewer.Size = new System.Drawing.Size(550, 96);
+            this.rtbFolderViewer.TabIndex = 1;
+            this.rtbFolderViewer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbFolderViewer);
+            this.Controls.Add(this.btnOpenFolderBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btnOpenFolderBrowser;
+        private System.Windows.Forms.RichTextBox rtbFolderViewer;
+    }
+}
+
---
+++ Activity_4.1.1/FileViewerForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity_4._1._1
+{
+    public partial class FileViewerForm : Form
+    {
+        private System.Windows.Forms.FolderBrowserDialog fbd1;
+
+        public FileViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFolderBrowser_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderPath = folderDlg.SelectedPath;
+                rtbFolderViewer.Text = folderPath;
+            }
+
+            FileStream outputFile = new FileStream("myfile.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outputFile);
+
+            writer.WriteLine(folderPath);
+            writer.Close();
+            outputFile.Close();
+        }
+    }
+}
